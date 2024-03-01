c459246c15cf0c0dde4c616f3a5cdf5951c50bfc
thefrankk <105942735+thefrankk@users.noreply.github.com>
2024-03-01 18:15:38 -0300

    add data

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import bluetooth_low_energy_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  BluetoothLowEnergyDarwin.register(with: registry.registrar(forPlugin: "BluetoothLowEnergyDarwin"))
 }
